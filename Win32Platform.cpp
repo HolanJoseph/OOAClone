cbb1132f371161a299c54b65dcf8e3c8d92245ab
Joseph Holan <holanjoseph@gmail.com>
2016-02-03 01:34:16 -0600

    IDK there was a change

--- Win32Platform.cpp
+++ Win32Platform.cpp
@@ -1,7 +1,52 @@
 #include <windows.h>
 #include "Types.h"
 
-INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow)
+
+
+LRESULT CALLBACK Win32WindowCallback( HWND windowHandle, UINT message, WPARAM wParam, LPARAM lParam )    
+{
+	switch (message)
+	{
+	case WM_CREATE: // Initialize the window. 
+	{
+		return 0;
+		break;
+	}
+
+	case WM_PAINT: // Paint the window's client area.
+	{
+		return 0;
+		break;
+	}
+
+	case WM_SIZE: // Set the size and position of the window.
+	{
+		return 0;
+		break;
+	}
+
+	case WM_DESTROY: // Clean up window-specific data objects. 
+	{
+		return 0;
+		break;
+	}
+
+	// 
+	// Process other messages. 
+	// 
+
+	default:
+	{
+		return DefWindowProc(windowHandle, message, wParam, lParam);
+	}
+	}
+
+	return 0;
+}
+
+
+
+INT WINAPI WinMain(HINSTANCE instanceHandle, HINSTANCE deadArg, PSTR commandLineArgsUnicode, INT showOnStartup)
 {
 	return 0;
 }
