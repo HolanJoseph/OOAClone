86a306c664341ae5ba28528ab8f40c331ece6a2e
Joseph Holan <holanjoseph@gmail.com>
2016-03-04 22:29:22 -0600

    ADD: Load3DModel, SplitStringOnCharacter functions. Load3DModel loads the vertex and index data from .obj files

--- Game.cpp
+++ Game.cpp
@@ -13,7 +13,97 @@
 #include "DebugAPI.h"
 #include "InputAPI.h"
 #include "FileAPI.h"
+#include "StringAPI.h"
 
+//NOTE: Loads as vec3
+//NOTE: indices are stored as U32s if larger is needed change this
+//pointer to vertices
+//numVerts
+//pointer to indices
+//numIndices
+bool Load3DModel(char* filename, F32** out_vertices, U64* out_numberOfVertices, U32** out_indices, U64* out_numberOfIndices)
+{
+	GetFileSizeReturnType fs = GetFileSize(filename);
+	char* fileContents = (char*)malloc(sizeof(char)*fs.fileSize);
+	ReadFileReturnType fr = ReadFile(filename, fileContents, fs.fileSize, 0);
+
+	if (!fs.fileExists || fr.errorEncountered)
+	{
+		return false;
+	}
+
+	U64 numberOfLines = 0;
+	U64* fileLineLengths = NULL;
+	char** fileLines = SplitStringOnCharacter(fileContents, fr.numberOfBytesRead, '\n', &numberOfLines, &fileLineLengths);
+
+	F32 * vertices = (F32*)malloc(sizeof(F32) * numberOfLines * 3); // 3 components per vertex
+	U64 numberOfVertices = 0;
+
+	U32* indices = (U32*)malloc(sizeof(U32) * numberOfLines * 3); // 3 indices per line
+	U64 numberOfIndices = 0;
+
+	for (U64 i = 0; i < numberOfLines; ++i)
+	{
+		if (fileLineLengths[i] > 0)
+		{
+			U64 numberOfComponents = 0;
+			U64* lineComponentLengths = NULL;
+			char** lineComponents = SplitStringOnCharacter(fileLines[i], fileLineLengths[i], ' ', &numberOfComponents, &lineComponentLengths);
+			if (lineComponents[0][0] == 'v' && lineComponentLengths[0] == 2)
+			{
+				// Process vertex
+				vertices[numberOfVertices++] = StringToF32(lineComponents[1]); // x
+				vertices[numberOfVertices++] = StringToF32(lineComponents[2]); // y
+				vertices[numberOfVertices++] = StringToF32(lineComponents[3]); // z
+			}
+			else if (lineComponents[0][0] == 'f')
+			{
+				// Process face indices
+				U64 faceVertex1NumberOfIndices = 0;
+				U64* faceVertex1IndiceLengths = NULL;
+				char** faceVertex1Indices = SplitStringOnCharacter(lineComponents[1], lineComponentLengths[1], '/', &faceVertex1NumberOfIndices, &faceVertex1IndiceLengths);
+				indices[numberOfIndices++] = StringToU32(faceVertex1Indices[0]);
+
+				U64 faceVertex2NumberOfIndices = 0;
+				U64* faceVertex2IndiceLengths = NULL;
+				char** faceVertex2Indices = SplitStringOnCharacter(lineComponents[2], lineComponentLengths[2], '/', &faceVertex2NumberOfIndices, &faceVertex2IndiceLengths);
+				indices[numberOfIndices++] = StringToU32(faceVertex2Indices[0]);
+
+				U64 faceVertex3NumberOfIndices = 0;
+				U64* faceVertex3IndiceLengths = NULL;
+				char** faceVertex3Indices = SplitStringOnCharacter(lineComponents[3], lineComponentLengths[3], '/', &faceVertex3NumberOfIndices, &faceVertex3IndiceLengths);
+				indices[numberOfIndices++] = StringToU32(faceVertex3Indices[0]);
+
+				free(faceVertex1Indices);
+				free(faceVertex1IndiceLengths);
+
+				free(faceVertex2Indices);
+				free(faceVertex2IndiceLengths);
+
+				free(faceVertex3Indices);
+				free(faceVertex3IndiceLengths);
+			}
+			
+			free(lineComponents);
+			free(lineComponentLengths);
+
+		}
+	}
+
+	vertices = (F32*)realloc(vertices, sizeof(F32)* numberOfLines * 3);
+	indices = (U32*)realloc(indices, sizeof(U32)* numberOfLines);
+
+	*out_vertices = vertices;
+	*out_numberOfVertices = numberOfVertices;
+	*out_indices = indices;
+	*out_numberOfIndices = numberOfIndices;
+
+	free(fileLines);
+	free(fileLineLengths);
+
+	free(fileContents);
+	return true;
+}
 
 struct Rectangle
 {
@@ -1991,6 +2081,24 @@ void CollisionSupportsTests()
 
 void CollisionDetection()
 {
+	F32* cubeVertices = NULL;
+	U64 numberOfCubeVertices = 0;
+	U32* cubeIndices = NULL;
+	U64 numberOfCubeIndices = 0;
+	Load3DModel("Assets/3D/Cube.obj", &cubeVertices, &numberOfCubeVertices, &cubeIndices, &numberOfCubeIndices);
+
+	F32* capsuleWallVertices = NULL;
+	U64 numberOfCapsuleWallVertices = 0;
+	U32* capsuleWallIndices = NULL;
+	U64 numberOfCapsuleWallIndices = 0;
+	Load3DModel("Assets/3D/CapsuleWall.obj", &capsuleWallVertices, &numberOfCapsuleWallVertices, &capsuleWallIndices, &numberOfCapsuleWallIndices);
+
+	F32* sphereVertices = NULL;
+	U64 numberOfSphereVertices = 0;
+	U32* sphereIndices = NULL;
+	U64 numberOfSphereIndices = 0;
+	Load3DModel("Assets/3D/sphere.obj", &sphereVertices, &numberOfSphereVertices, &sphereIndices, &numberOfSphereIndices);
+
 
 	bool collisionDetected = false;
 
---
+++ StringAPI.h
@@ -0,0 +1,55 @@
+#pragma once
+// NOTE: At this point this is basically a shell
+
+#include "Types.h"
+
+#include <cstdlib>
+
+inline I32 StringToI32(char* string)
+{
+	I32 result = atoi(string);
+	return result;
+}
+
+inline U32 StringToU32(char* string)
+{
+	U32 result = strtoul(string, NULL, 0);
+	return result;
+}
+
+inline F32 StringToF32(char* string)
+{
+	F32 result = strtof(string, NULL);
+	return result;
+}
+
+inline char** SplitStringOnCharacter(char* string, U64 stringLength, char splitCharacter, U64* out_numberOfSplits, U64** out_lineLengths)
+{
+	char** stringComponents = (char**)malloc(sizeof(char*) * stringLength);
+	U64* stringComponentLengths = (U64*)malloc(sizeof(U64) * stringLength);
+
+	char* stringFront = string;
+	U64 numberOfSplits = 0;
+	for (U64 i = 0; i < stringLength; ++i)
+	{
+		if (string[i] == splitCharacter)
+		{
+			string[i] = '\0';
+			stringComponents[numberOfSplits] = stringFront;
+			stringComponentLengths[numberOfSplits] = (&(string[i]) - stringFront + 1) / sizeof(char);
+			stringFront = &(string[i + 1]);
+			++numberOfSplits;
+		}
+	}
+
+	stringComponents[numberOfSplits] = stringFront;
+	stringComponentLengths[numberOfSplits] = (&(string[stringLength]) - stringFront) / sizeof(char);
+	++numberOfSplits;
+
+	stringComponents = (char**)realloc(stringComponents, sizeof(char*) * numberOfSplits);
+ 	stringComponentLengths = (U64*)realloc(stringComponentLengths, sizeof(U64) * numberOfSplits);
+
+	*out_numberOfSplits = numberOfSplits;
+	*out_lineLengths = stringComponentLengths;
+	return stringComponents;
+}
